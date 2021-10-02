4f0439667b68f97c99db66f9debd4c8f1559fb6e
Cullen Lee <a410615903@gmail.com>
2021-10-02 01:28:59 -0700

    updated metadata feature

--- metadata.js
+++ metadata.js
@@ -1,9 +1,6 @@
 const tiny = require('tiny-json-http');
 const { v5: uuidv5 } = require('uuid');
 
-const args = { owner: "keyskull", repo: "publish" };
-
-
 async function getMetaDatabyUrl(url) {
   var json = {};
   try {
@@ -15,8 +12,12 @@ async function getMetaDatabyUrl(url) {
   return json;
 }
 
-function logging(eventName, messages) {
-  console.log('['+Date.now().toDateString() +'] '+ eventName+ ': ' + messages)
+function logging(eventName, messages,level) {
+  var lv = "INFO"
+  if(level){
+    lv = level;
+  }
+  console.log('[' + new Date(Date.now()).toJSON() +'] '+ lv+ ': ' + eventName + ': ' + messages);
   
 }
 
@@ -29,91 +30,149 @@ async function exiamineMetaData(metadata) {
   if (!metadata['trash']) metadata['trash'] = new Array();
   if (!metadata['logging']) metadata['logging'] = new Array();
 
-  logging('exiamineMetaData','exiamined metadata structure.');
+  logging('exiamineMetaData', 'exiamined metadata structure.');
+  return metadata;
+}
+
+
+function unrecordedFileAction(file_name, metadata) {
+  logging('UnrecordedFileAction', 'started a unrecordedFileAction');
+
+
+  logging('UnrecordedFileAction', 'created file name: ' + file_name);
+  const uuid = uuidv5(file_name, uuidv5.URL);
+  if (metadata['articles'][uuid]) {
+    if (revise_time > -1) metadata['articles'][uuid]['revise_time'] = revise_time + 1;
+    else metadata['articles'][uuid]['revise_time'] = 1;
+  }
+  else {
+    const json = '{"' + uuidv5(file_name, uuidv5.URL) + '" : {"path":"' + file_name + '"' +
+      ',"title": "' + file_name + '"' +
+      ',"revise_time": 0 ' +
+      ',"authors": [] ' +
+      ',"tags": [] ' +
+      ',"used_names": [] ' +
+      ',"last_action": "unknown"' +
+      ',"created_timestamp":' + Date.now() +
+      ',"updated_timestamp":' + Date.now() +
+      "}}";
+    logging('UnrecordedFileAction', 'created file json detail: ' + json);
+
+    metadata['articles'] = JSON.parse(json);
+
+    logging('UnrecordedFileAction', 'recorded a created file: ' + file_name);
+  }
+
+
   return metadata;
 }
 
 function createdAction(files_detail, metadata) {
-  files_detail['status']["added"].forEach(
-    function (value) {
-      metadata['articles'] = JSON.parse("{" + uuidv5(value, uuidv5.URL) + ": {'path':" + value +
-        ",'title': " + value +
-        ",'revise_time': 0 " +
-        ",'authors': [] " +
-        ",'tags': [] " +
-        ",'used_names': [] " +
-        ",'last_action': added" +
-        ",'created_timestamp':" + files_detail['timestamp'] +
-        ",'updated_timestamp': " + files_detail['timestamp'] +
-        "}}");
+  logging('createdAction', 'started a createdAction');
+  logging('createdAction', 'counts of created files: ' + files_detail['status']["added"].length);
+  logging('createdAction', 'created files infomation: ' + files_detail['status']["added"]);
 
-        logging('createdAction','recorded a created file: '+ value);
-    })
+  files_detail['status']["added"].forEach(function (value) {
+    if (value != '') {
+
+      const json = '{"' + uuidv5(value, uuidv5.URL) + '" : {"path":"' + value + '"' +
+        ',"title": "' + value + '"' +
+        ',"revise_time": 0 ' +
+        ',"authors": [] ' +
+        ',"tags": [] ' +
+        ',"used_names": [] ' +
+        ',"last_action": "added"' +
+        ',"created_timestamp":' + files_detail['timestamp'] +
+        ',"updated_timestamp":' + files_detail['timestamp'] +
+        "}}";
+      logging('createdAction', 'created file json detail: ' + json);
+
+      metadata['articles'] = JSON.parse(json);
+
+      logging('createdAction', 'recorded a created file: ' + value);
+    }
+  })
 
   return { files_detail, metadata };
 }
 
 function modifiedAction(files_detail, metadata) {
-  files_detail['status']["modified"].forEach(
-    function (value) {
-      var uuid = uuidv5(value, uuidv5.URL);
-      if (!metadata['articles'][uuid])
-        createdAction();
+  logging('modifiedAction', 'started a modifiedAction');
+
+  files_detail['status']["modified"].forEach(function (value) {
+    if (value != '') {
+
+      const uuid = uuidv5(value, uuidv5.URL);
+
+      if (!metadata['articles'][uuid]) {
+        logging('modifiedAction', 'unknown modified action file: ' + value);
+        unrecordedFileAction(value, metadata)
+      }
       else {
-        revise_time = Number.parseInt(metadata['articles'][uuid]['revise_time']);
-        if (revise_time)
+        const revise_time = Number.parseInt(metadata['articles'][uuid]['revise_time']);
+        if (revise_time > -1)
           metadata['articles'][uuid]['revise_time'] = revise_time + 1;
         else metadata['articles'][uuid]['revise_time'] = 1;
         metadata['articles'][uuid]['updated_timestamp'] = Date.now();
-        metadata['articles'][uuid]['last_action'] = 'modified'
+        metadata['articles'][uuid]['last_action'] = "modified";
+        logging('modifiedAction', 'recorded a modified file: ' + value);
       }
-    
-      logging('modifiedAction','recorded a modified file: '+ value);
-    });
+    }
+  });
   return { files_detail, metadata };
 }
 
 function removedAction(files_detail, metadata) {
-  files_detail['status']["removed"].forEach(
-    function (value) {
+  logging('removedAction', 'started a removedAction');
+
+  files_detail['status']["removed"].forEach(function (value) {
+
+    if (value != '') {
+
       const uuid = uuidv5(value, uuidv5.URL);
       if (metadata['articles'][uuid]) {
         metadata['articles'][uuid]['last_action'] = 'removed';
         metadata['articles'][uuid]['updated_timestamp'] = Date.now();
-        metadata['trash'] = metadata['articles'][uuid];
+        metadata['trash'][uuid] = metadata['articles'][uuid];
         delete metadata['articles'][uuid];
-
-        logging('removedAction','recorded a removed file: '+ value);
-
+        logging('removedAction', 'recorded a removed file: ' + value);
       } else {
-        logging('removedAction',"doesn't find a file: "+ value);
+        logging('removedAction', "doesn't find a file: " + value);
+        unrecordedFileAction(value, metadata) 
       }
-    });
+
+    }
+  });
   return { files_detail, metadata };
 }
 
 function renamedAction(files_detail, metadata) {
+  logging('renamedAction', 'started a renamedAction');
 
   files_detail['status']["renamed"].forEach(
     function (value) {
-      const uuid = uuidv5(value.previous_filename, uuidv5.URL);
-      if (!metadata['articles'][uuid]) createdAction();
-      else {
-        const revise_time = Number.parseInt(metadata['articles'][uuid]['revise_time']);
+      if (value != '') {
+        const uuid = uuidv5(value.previous_filename, uuidv5.URL);
+        if (!metadata['articles'][uuid]) {
+          logging('renamedAction', 'unknown modified action file: ' + value);
+          unrecordedFileAction(value, metadata) 
+        }
+        else {
+          const revise_time = Number.parseInt(metadata['articles'][uuid]['revise_time']);
 
-        metadata['articles'][uuid]['revise_time'] = revise_time + 1;
-        if (!metadata['articles'][uuid]['used_names']) metadata['articles'][uuid]['used_names'] = new Array();
-        metadata['articles'][uuid]['used_names'].push(value.previous_filename);
-        metadata['articles'][uuid]['updated_timestamp'] = Date.now();
+          metadata['articles'][uuid]['revise_time'] = revise_time + 1;
+          if (!metadata['articles'][uuid]['used_names']) metadata['articles'][uuid]['used_names'] = new Array();
+          metadata['articles'][uuid]['used_names'].push(value.previous_filename);
+          metadata['articles'][uuid]['updated_timestamp'] = Date.now();
 
-        metadata['articles'][uuidv5(value.filename, uuidv5.URL)] = metadata['articles'][uuid];
-        delete metadata['articles'][uuid];
+          metadata['articles'][uuidv5(value.filename, uuidv5.URL)] = metadata['articles'][uuid];
+          delete metadata['articles'][uuid];
+          logging('renamedAction', 'recorded a rename file frome' + value.previous_filename + 'to' + value.filename);
+        }
       }
-
-      logging('renamedAction','recorded a rename file frome'+ value.previous_filename +'to'+ value.filename);
-
     });
-  return { files_detail, metadata };
+
+    return { files_detail, metadata };
 }
 
 
@@ -129,11 +188,13 @@ exports.generateMetaData = async function (files_detail, url) {
     .then(data => removedAction(data.files_detail, data.metadata));
 
 
-  Object.keys(files_detail['status']).forEach(function (key) {
-    console.log('Key : ' + key + ', Value : ' + files_detail['status'][key])
-  })
+  // Object.keys(files_detail['status']).forEach(function (key) {
+  //   console.log('Key : ' + key + ', Value : ' + files_detail['status'][key])
+  // })
 
-  return result;
+  logging('generateMetaData', 'result:' + JSON.stringify(result.metadata));
+
+  return result.metadata;
 }
 
 
